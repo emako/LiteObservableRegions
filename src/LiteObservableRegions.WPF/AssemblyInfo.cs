b3f9b2198bd2aa5e872493b1ba8070c955addfdd
ema <mccoy39082@163.com>
2026-02-14 10:23:21 +0800

    Add XAML Xmlns assembly info

---
+++ src/LiteObservableRegions.WPF/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://schemas.github.com/liteobservableregions/2026/xaml", "r")]
+[assembly: XmlnsDefinition("http://schemas.github.com/liteobservableregions/2026/xaml", "LiteObservableRegions")]
